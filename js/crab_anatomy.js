5ddc9c32168a5673c7e9c6481a28979c6f67b236
William B <william@barkoffusa.com>
2018-02-11 11:21:15 -0500

    swtudd

---
+++ js/crab_anatomy.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    document.getElementById("crab-eyes").addEventListener("click", () => {$('#eyes-modal').modal()});
+    document.getElementById("crab-legs-left").addEventListener("click", () => {$('#legs-modal').modal()});
+    document.getElementById("crab-legs-right").addEventListener("click", () => {$('#legs-modal').modal()});
+    document.getElementById("crab-pincher-left").addEventListener("click", () => {$('#pinchers-modal').modal()});
+    document.getElementById("crab-pincher-right").addEventListener("click", () => {$('#pinchers-modal').modal()});
+});
