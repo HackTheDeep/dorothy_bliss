4b6888cb23d847e8a521623251ddef6a06f4dd3a
Alex Studer <astuder@notetoscreen.com>
2018-02-11 10:47:12 -0500

    evil crab can select food and move to it

--- js/water_balance.js
+++ js/water_balance.js
@@ -18,12 +18,16 @@ var canvas;
 var ctx;
 
 var objects = [];
+var pool1food = [];
+var pool2food = [];
+
+var foodTimer = 0;
 
 var crab = new Crab();
 crab.x = crab.y = 300;
 objects.push(crab);
 
-var evilCrab = new EvilCrab();
+var evilCrab = new EvilCrab(crab);
 evilCrab.x = evilCrab.y = 400;
 objects.push(evilCrab);
 
@@ -41,8 +45,73 @@ pool2.a = 112.5;
 pool2.b = 52;
 objects.push(pool2);
 
+var updateArray = function(array) {
+	array.forEach(function(object) {
+		object.update({
+			pool1: pool1,
+			pool2: pool2,
+			pool1food: pool1food,
+			pool2food: pool2food
+		});
+	});
+};
+
+var drawArray = function(array, ctx, images) {
+	array.forEach(function(object) {
+		object.draw(ctx, images);
+	});
+};
+
+var generateFood = function(food, pool, poolArray) {
+	var food = new Food();
+	// from https://math.stackexchange.com/questions/22064/calculating-a-point-that-lies-on-an-ellipse-given-an-angle
+	var theta = Math.floor((Math.random()*360) + 1);
+	var quadrant = Math.floor((Math.random()*4) + 1);
+	var a = pool.a;
+	var b = pool.b;
+	var x = (a * b) / (Math.sqrt(Math.pow(b, 2) + (Math.pow(a, 2) * Math.pow(Math.tan(theta), 2))));
+	var y = (a * b) / (Math.sqrt(Math.pow(a, 2) + (Math.pow(b, 2) / Math.pow(Math.tan(theta), 2))));
+	if (quadrant == 1) {
+		x = Math.abs(x);
+		y = Math.abs(y);
+	} else if (quadrant == 2) {
+		x = -Math.abs(x);
+		y = Math.abs(y);
+	} else if (quadrant == 3) {
+		x = -Math.abs(x);
+		y = -Math.abs(y);
+	} else if (quadrant == 4) {
+		x = Math.abs(x);
+		y = -Math.abs(y);
+	}
+	food.x = pool.x + x - (food.width / 2);
+	food.y = pool.y + y - food.height;
+	poolArray.push(food);
+};
+
 var loop = function() {
+	// food generation
+	if (pool1food.length < 3) {
+		if (foodTimer > 60*3) {
+			generateFood(pool1food, pool1, pool1food);
+		} else {
+			foodTimer++;
+		}
+	}
+	if (pool2food.length < 3) {
+		if (foodTimer > 60*3) {
+			generateFood(pool2food, pool2, pool2food);
+		} else {
+			foodTimer++;
+		}
+	}
+
 	// update
+	updateArray(objects);
+	updateArray(pool1food);
+	updateArray(pool2food);
+
+	// input
 	if (keys[37]) { // left
 		crab.x--;
 	}
@@ -55,15 +124,14 @@ var loop = function() {
 	if (keys[40]) { // down
 		crab.y++;
 	}
-	objects.forEach(function(object) {
-		object.update();
-	});
 
 	// draw
 	ctx.drawImage(images.sand, 0, 0);
-	objects.forEach(function(object) {
-		object.draw(ctx, images);
-	});
+
+	drawArray(objects, ctx, images);
+	drawArray(pool1food, ctx, images);
+	drawArray(pool2food, ctx, images);
+
 	ctx.fillStyle = "black";
 	ctx.font = "14px Arial";
 	ctx.fillText("Salinity: " + crab.salinity + "%", 10, 24); 
@@ -86,6 +154,13 @@ $(document).ready(function() {
 
 		window.addEventListener("keydown", this.keychange.bind(this, true), false);
 		window.addEventListener("keyup", this.keychange.bind(this, false), false);
+
+		while (pool1food.length < 3) {
+			generateFood(pool1food, pool1, pool1food);
+		}
+		while (pool2food.length < 3) {
+			generateFood(pool2food, pool2, pool2food);
+		}
 
 		loop();
 	});
--- js/water_balance_objects.js
+++ js/water_balance_objects.js
@@ -3,14 +3,16 @@ function Crab() {
 	this.x = 0;
 	this.y = 0;
 	this.salinity = 70;
-	this.salinityTimer = 10;
+	this.salinityTimer = 30;
+	this.width = 125;
+	this.height = 94;
 }
 
 Crab.prototype.update = function() {
 	this.salinityTimer--;
 	if (this.salinityTimer <= 0) {
 		this.salinity--;
-		this.salinityTimer = 10;
+		this.salinityTimer = 30;
 	}
 };
 
@@ -22,20 +24,66 @@ Crab.prototype.draw = function(ctx, images) {
 	} else if (this.salinity > 50) {
 		crabImage = images.crab_bulging2;
 	}
+	
+	if (this.evil) {
+		crabImage = images.crab_evil;
+	}
 
-	ctx.drawImage(crabImage, this.x, this.y, 125, 94);
+	ctx.drawImage(crabImage, this.x, this.y, this.width, this.height);
 };
 
 // EvilCrab
 EvilCrab.prototype = new Crab();
 EvilCrab.prototype.constructor = EvilCrab;
 
-function EvilCrab() {
+function EvilCrab(goodCrab) {
 	Crab.call(this);
+	this.evil = true;
+	this.goodCrab = goodCrab;
+	this.targetPool = -1;
+	this.targetFoodIndex = -1;
+	this.eatTimer = 0;
+	this.speed = 1;
 }
 
-EvilCrab.prototype.update = function() {
-	//console.log("wowowow i am evil");
+EvilCrab.prototype.update = function(scene) {
+	// target select
+	if (this.targetPool == -1) {
+		// find food to eat
+		this.targetPool = Math.floor((Math.random() * 2) + 1);
+		this.targetFoodIndex = Math.floor((Math.random() * 3));
+	}
+
+	var targetPoolObject = (this.targetPool == 1 ? scene.pool1 : scene.pool2);
+	var targetPoolFood = (this.targetPool == 1 ? scene.pool1food : scene.pool2food);
+	var targetFood = targetPoolFood[this.targetFoodIndex];
+
+	var foodCenterX = targetFood.x + (targetFood.width / 2);
+	var foodCenterY = targetFood.y + (targetFood.height / 2);
+	var crabCenterX = this.x + (this.width / 2);
+	var crabCenterY = this.y + (this.height / 2);
+
+	// target movement
+	if (foodCenterX > crabCenterX) {
+		this.x += this.speed;
+	} else if (foodCenterX < crabCenterX) {
+		this.x -= this.speed;
+	}
+
+	if (foodCenterY > crabCenterY) {
+		this.y += this.speed;
+	} else if (foodCenterY < crabCenterY) {
+		this.y -= this.speed;
+	}
+
+	// target eat
+	if (Math.abs(foodCenterX - crabCenterX) < 3 && Math.abs(foodCenterY - crabCenterY) < 3) {
+		if (this.eatTimer > 3*60) {
+
+		} else {
+			this.eatTimer++;
+		}
+	}
 };
 
 // Pool
@@ -53,4 +101,26 @@ Pool.prototype.draw = function(ctx, images) {
 	ctx.fillStyle = "rgba(163, 205, 202, 0.7)";
 	ctx.ellipse(this.x, this.y, this.a, this.b, 0, 0, 2 * Math.PI);
 	ctx.fill();
+};
+
+// Food
+function Food() {
+	this.x = this.y = 0;
+	this.image = (Math.random() > 0.5 ? images.grass1 : images.grass2);
+	this.origWidth = (this.image == images.grass1 ? 562 : 971);
+	this.origHeight = (this.image == images.grass1 ? 1020 : 2051);
+	this.width = 35;
+	this.height = this.origHeight * (this.width/this.origWidth);
+}
+
+Food.prototype.update = function() {
+
+};
+
+Food.prototype.draw = function(ctx, images) {
+	ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+	// ctx.beginPath();
+	// ctx.fillStyle = "black"; //rgba(163, 205, 202, 0.7)";
+	// ctx.ellipse(this.x, this.y, 3, 3, 0, 0, 2 * Math.PI);
+	// ctx.fill();
 };
